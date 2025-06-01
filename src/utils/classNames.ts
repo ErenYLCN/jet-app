2be511a1ff54ea88863909bfc96fceaf0f248eaf
Eren Yalçın <yalcineren97@gmail.com>
2025-06-02 00:05:52 +0200

    feat: improve `cn` function

--- src/utils/classNames.ts
+++ src/utils/classNames.ts
@@ -1,10 +1,28 @@
 /**
  * Concatenates class names.
- * @param {...(string | boolean | undefined)} args - Class names to concatenate.
+ * @param {...(string | boolean | undefined | Record<string, unknown>)} args - Class names to concatenate.
  * @returns {string} A string of space-separated class names.
  */
-function cn(...args: (string | boolean | undefined)[]): string {
-  return args.filter(Boolean).join(" ");
+function cn(
+  ...args: (string | boolean | undefined | Record<string, unknown>)[]
+): string {
+  const classes: string[] = [];
+
+  for (const arg of args) {
+    if (!arg) continue;
+
+    if (typeof arg === "string") {
+      classes.push(arg);
+    } else if (typeof arg === "object") {
+      for (const [key, value] of Object.entries(arg)) {
+        if (value) {
+          classes.push(key);
+        }
+      }
+    }
+  }
+
+  return classes.join(" ");
 }
 
 export default cn;
